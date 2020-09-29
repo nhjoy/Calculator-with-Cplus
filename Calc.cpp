9ec0ec027429199b0baa4eb05668cfd1ad2e7e16
nhjoy <nhjoy7232@gmail.com>
2020-09-29 18:53:07 +0800

    line of codes added in Calc

--- Calc.cpp
+++ Calc.cpp
@@ -1,6 +1,18 @@
 #include <iostream>  // user i/o
 #include "Calc.h"    // declaring my header
-#include <stddef.h>  // defines various variable types and macros
+#include "stddef.h"  // defines various variable types and macros
 #include <cmath>     //declares a set of functions to compute common mathematical operations and transformations
 
 
+
+Calc::Calc(int values):storedExpressions(new std::string[values]), stored(0){
+}
+
+Calc::Calc(const Calc &rhs){
+}
+
+Calc::~Calc(){
+}
+
+const Calc &Calc::operator=(const Calc &rhs){
+}
--- Calc.h
+++ Calc.h
@@ -11,8 +11,16 @@ private:
     size_t store;
 public:
     Calc(int val);
-    // ~Calc();
-
+    /*
+        Big 3
+        used for memory management
+        dynamically allocated the memory and destroy when not used
+        when one element is called assued other two elements are needed
+        thus big 3
+    */
+    Calc(const Calc &rhs); // constructor, & means memory address of rhs
+    ~Calc(); // destructor- removes garbage
+    const Calc &operator=(const Calc &rhs);  // copy assignment- return const Calc object and overload "=" operator
     // methods
     double process (std::string userInput, double prevValue);
     void print();
